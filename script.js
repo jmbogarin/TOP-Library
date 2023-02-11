d85fdb72b606c2e9e8336fa8aa88c9e2d5825f62
jmbogarin <jmbogarin@users.noreply.github.com>
2023-02-11 20:12:07 -0300

    Add func for removing a book

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 "use strict";
 
+const newBookBtn = document.querySelector('#btn-newbook');
 const addBookBtn = document.querySelector('#btn-addbook');
-const formNewbook = document.querySelector('#form-newbook div')
+const formNewbook = document.querySelector('#form-newbook div');
+
+const modal = document.querySelector('#modal');
+
+const main = document.querySelector("#main");
 
 let myLibrary = [];
 
@@ -10,7 +15,8 @@ class Book {
         this.title = title;
         this.author = author;
         this.pages = pages;
-        this.read = read
+        this.read = read;
+        this.numID = myLibrary.length;
     }
 }
 
@@ -22,12 +28,52 @@ function getBookData() {
     return new Book(...values);
 }
 
+function refreshLibrary() {
+    main.innerHTML = ""
+    for (const book of myLibrary) {
+        const bookHTML = document.createElement("div")
+        bookHTML.className = "book"
+        bookHTML.innerHTML = 
+            `
+            <span class="btn-del" numID="${book.numID}"> x </span>
+            <div class="title">${book.title}</div>
+            <div class="author">${book.author}</div>
+            <div class="pages">${book.pages} pages</div>
+            <label>Read<input type="checkbox" ${(book.read === "on") ? "checked" : ""}></label>
+            `
+        main.appendChild(bookHTML);
+    }
+}
+
+function deleteBook (index) {
+    myLibrary.splice(index, 1);
+    for (const book in myLibrary) {
+        myLibrary[book].numID = book
+    }
+    refreshLibrary();
+}
+
 function addBookToLibrary(event) {
     event.preventDefault();
     const book = getBookData();
-    console.dir(book)
+    myLibrary.push(book);
+    modal.classList.add("hidden");
+    refreshLibrary();
+    for (const el of formNewbook.children) {
+        el.value = ""
+        el.checked = false
+    }
 }
 
-
-
 addBookBtn.addEventListener('click', addBookToLibrary)
+
+newBookBtn.addEventListener('click', () => {modal.classList.remove("hidden")})
+
+modal.addEventListener('click', (e) => {if (e.target.id == 'modal') {modal.classList.add("hidden")}})
+
+main.addEventListener('click', (e) => {
+    if (e.target.className === "btn-del") {
+        deleteBook(e.target.attributes.numID.value)
+    }
+    
+})
